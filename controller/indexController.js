858b167c0fe67bd0c65d9328ff6243bf24b37e7b
Sofia Rodriguez Vazquez <sofi_rv@hotmail.com>
2020-06-26 13:36:50 -0300

    Sofia ok

---
+++ controller/indexController.js
@@ -0,0 +1,29 @@
+const fs= require('fs');
+const path= require('path');
+const bcrypt= require ('bcryptjs');
+
+let indexController={
+    home: (req, res)=> {
+        res.render('index', {
+            titulo: "Proyecto",
+            mensaje: '3 y 6 cuotas sin interés | envío gratis en compras superiores a $1500'
+        })
+    },
+    recicla: (req,res)=>{
+        res.render('porquereciclado',{
+            titulo: "Proyecto",
+            mensaje: '3 y 6 cuotas sin interés | envío gratis en compras superiores a $1500'
+        })
+    },
+    covid19: (req,res)=>{
+        res.render('covid19')
+    },
+    mujer: (req,res)=>{
+        res.render('mujer',{
+            titulo: "Proyecto",
+            mensaje: '3 y 6 cuotas sin interés | envío gratis en compras superiores a $1500'
+        })
+    }
+}
+
+module.exports=indexController;
