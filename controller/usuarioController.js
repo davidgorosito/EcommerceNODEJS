0fc6304c47516d41edd8ae74ce5364070cb988e4
davidgorosito <davidgorositouniversidad@gmail.com>
2020-07-17 10:35:54 -0300

    arreglo del model

--- controller/usuarioController.js
+++ controller/usuarioController.js
@@ -1,10 +1,6 @@
 const fs= require('fs');
 const path = require('path')
-<<<<<<< HEAD
 const bcrypt= require('bcryptjs');
-=======
-const bcrypt=require('bcryptjs');
->>>>>>> db466c91d41949b61211057131aea357e9f5988c
 const { json }= require('express');
 const { validationResult } = require('express-validator');
 
